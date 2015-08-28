d34398ddc8bbc575e9e7a37a7fee54fdad8ad94f
Samuel Neves <sneves@dei.uc.pt>
2015-08-28 04:41:15 +0100

    norx6461/neon -> new init && v2.0

--- utils/bench.c
+++ utils/bench.c
@@ -10,6 +10,9 @@
    You should have received a copy of the CC0 Public Domain Dedication along with
    this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 */
+
+#define _GNU_SOURCE
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
