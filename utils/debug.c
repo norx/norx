3121999d17857e49514b3017331fde044cfedb4a
Philipp Jovanovic <philipp@jovanovic.io>
2015-09-06 15:28:51 +0200

    another IP update

--- utils/debug.c
+++ utils/debug.c
@@ -1,3 +1,18 @@
+/*
+   NORX reference source code package - reference C implementations
+
+   Written 2014-2015 by:
+
+        - Samuel Neves <sneves@dei.uc.pt>
+        - Philipp Jovanovic <jovanovic@fim.uni-passau.de>
+
+   To the extent possible under law, the author(s) have dedicated all copyright
+   and related and neighboring rights to this software to the public domain
+   worldwide. This software is distributed without any warranty.
+
+   You should have received a copy of the CC0 Public Domain Dedication along with
+   this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
