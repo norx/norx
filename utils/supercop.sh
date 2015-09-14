1bed4e374328a1267f15ce92f8ee710b6df6f149
Philipp Jovanovic <philipp@jovanovic.io>
2015-09-14 16:18:57 +0200

    update SUPERCOP script

--- utils/supercop.sh
+++ utils/supercop.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 #
-# NORX reference source code package - script to wrap up implementations for SUPERCOP
+# Script to wrap up implementations for SUPERCOP
 #
-# Written in 2014 by Philipp Jovanovic <jovanovic@fim.uni-passau.de>
-#
-# To the extent possible under law, the author(s) have dedicated all copyright
-# and related and neighboring rights to this software to the public domain
-# worldwide. This software is distributed without any warranty.
-#
-# You should have received a copy of the CC0 Public Domain Dedication along with
-# this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 DEST=crypto_aead
-VER=v1
+VER=v2
 
 if [ -d "$DEST" ]; then rm -rf $DEST; fi
 mkdir -p $DEST
 
-for DIR in ../norx*; do
+# copy stuff
+for DIR in ../norx{3241,3261,6441,6444,6461}; do
   SRC=$DIR
   BASE=$(basename $DIR)
   rsync -a $SRC $DEST --exclude={makefile,kat.h}
   mv $DEST/$BASE $DEST/$BASE$VER
 done;
 
+# activate SUPERCOP support
 if [ `uname` == "Darwin" ]; then
   find $DEST -type f -exec sed -i "" -e 's/defined(SUPERCOP)/1/g' '{}' \;
 else
